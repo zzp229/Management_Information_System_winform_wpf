44f000eb2bdbe9b437011c4948c06391772d03ab
“nongnong” <a13556632276@gmail.com>
2023-08-30 23:11:43 +0800

    实现登录的创建

---
+++ 医药管理系统wpf/医药管理系统wpf/FrmLogin.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace 医药管理系统wpf
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class FrmLogin : Window
+    {
+        public FrmLogin()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_Login_Click(object sender, RoutedEventArgs e)
+        {
+            FrmMain frmMain = new FrmMain();
+            frmMain.Show();
+        }
+    }
+}
